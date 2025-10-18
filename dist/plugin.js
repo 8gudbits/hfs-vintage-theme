2811a0ece075eea9aa9e5eeb594ab5288110607f
Noman <130601057+8gudbits@users.noreply.github.com>
2025-10-18 18:10:26 +0600

    v1.1

--- dist/plugin.js
+++ dist/plugin.js
@@ -1,7 +1,7 @@
-exports.version = 1.0
-exports.description = "Vintage theme by 8gudbits"
-exports.isTheme = "light"
-exports.preview = "https://github.com/8gudbits/hfs-vintage-theme/raw/main/preview.png"
-exports.apiRequired = 1
 exports.repo = "8gudbits/hfs-vintage-theme"
+exports.version = 1.1
+exports.description = "Vintage theme by 8gudbits"
+exports.preview = "https://github.com/8gudbits/hfs-vintage-theme/raw/main/preview.png"
+exports.isTheme = "light"
+exports.apiRequired = 1
 exports.frontend_css = "style.css"
